9e275d74fdb4b5557fd72e1c1e5f4492275648cc
Richard Duan <dxloader@gmail.com>
2018-09-07 21:52:48 +1000

    get func

--- get.js
+++ get.js
@@ -1,25 +1,23 @@
-import uuid from 'uuid';
-
 import * as dynamoDbLib from './libs/dynamodb-lib';
 import { success, failure } from './libs/response-lib';
 
 export async function main(event, context, callback) {
-  const data = JSON.parse(event.body);
-
   const params = {
     TableName: 'notes',
-    Item: {
+    Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: uuid.v1(),
-      content: data.content,
-      attachment: data.attachment,
-      createdAt: Date.now()
+      noteId: event.pathParameters.id
     }
   };
 
   try {
-    await dynamoDbLib.call('put', params);
-    callback(null, success(params.Item));
+    const result = await dynamoDbLib.call('get', params);
+
+    if (result.Item) {
+      callback(null, success(result.Item));
+    } else {
+      callback(null, failure({ status: false, error: 'Item not found.' }));
+    }
   } catch (e) {
     callback(null, failure({ status: false }));
   }
